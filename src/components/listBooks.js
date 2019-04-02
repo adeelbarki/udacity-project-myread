aa8289d3b87426a02f9bd463580525ef27c0e50f
Adeel Barki <adeelbarki@gmail.com>
2019-04-02 09:26:28 +0500

    update search style

--- src/components/listBooks.js
+++ src/components/listBooks.js
@@ -4,19 +4,36 @@ class ListBooks extends Component {
     state = {
         query: ''
     }
+    
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+    
     render() {
         const { query } = this.state
         const { books } = this.props
+
+        const showingBooks = query === ''
+        ? books
+        : books.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
+
         return (
-            <div className="'books-grid">
-                <ol className="">
-                    {books.map((book) => (
-                       <li key={book.title} className='books-grid'>
-                            <div className='book-avatar'
-                           style={{
-                               backgroundImage: `url(${book.imageLinks.thumbnail})`
-                            }}>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {showingBooks.map((book) => (
+                       <li key={book.title} className='book'>
+                        <div className="book-top">
+                            <div className='book-cover'
+                                style={{
+                                    width: 128, height: 193,
+                                    backgroundImage: `url(${book.imageLinks.thumbnail})`
+                                }}>
                             </div>
+                        </div>
                         <div className='book'>
                             <p className="book-title">{book.title}</p>
                             <p className="book-authors">{book.authors}</p>
