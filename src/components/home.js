f894f90fad98b0c495349bb770cc7826ec92f8ab
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-04-10 12:16:32 +0500

    Added get function to home page database

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,40 @@
-import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../utils/BooksAPI'
+import ListBooks from './listBooks';
 
 class Home extends Component {
+    state = {
+        books: [],
+        query: ''
+    }
+
+    componentDidMount(){
+        BooksAPI.getAll()
+            .then((books) => {
+                this.setState(() => ({
+                    books
+                }))
+            })
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
+    clearQuery = () => {
+        this.updateQuery('')
+    }
     render(){
+        const { query, books } = this.state
+
+        const showingBooks = query === ''
+        ? books
+        : books.filter((c) => (
+            c.title.toLowerCase().includes(query.toLowerCase())
+        ))
         return(
             <div className="list-books">
                 <div className="list-books-title">
@@ -15,6 +47,9 @@ class Home extends Component {
                             <h2 className="bookshelf-title">Want to Read</h2>
                             <h2 className="bookshelf-title">Read</h2>
                         </div>
+                        <ListBooks 
+                            showingBooks={showingBooks}
+                        />
                     </div>
                 </div>
                 <div className="open-search">
